a622b8e76440f6219f67508491da2a5497928bae
Ranjit Bobade <bobaderanjit37@gmail.com>
2024-01-11 23:14:45 +0530

    Connected all Pages

--- classapp/urls.py
+++ classapp/urls.py
@@ -1,8 +1,15 @@
 
 from django.urls import path
 from classapp import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    
+    path('',views.home,name='home'),
+    path('about/',TemplateView.as_view(template_name="about.html")),
+    path('cources/',TemplateView.as_view(template_name="courses.html")),
+    path('trainers/',TemplateView.as_view(template_name="trainers.html")),
+    path('pricing/',TemplateView.as_view(template_name="pricing.html")),
+    path('contact/',TemplateView.as_view(template_name="contact.html")),
+    path('login/',views.login),
+    path('signup/',views.signup),
 ]
--- classapp/views.py
+++ classapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request,'index.html')
+def home(request):
+    return render(request,'home.html')
+
+
+def login(request):
+    return render(request,'login.html')
+
+def signup(request):
+    return render(request,"signup.html")
